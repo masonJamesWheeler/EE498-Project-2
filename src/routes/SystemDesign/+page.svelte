61018f6d2f352feb6abd7b2765993023ad04b45d
Mason Wheeler <masewheeler@Masons-MacBook-Pro.local>
2023-12-08 12:01:17 -0800

    newest changes

---
+++ src/routes/SystemDesign/+page.svelte
@@ -0,0 +1,79 @@
+<script>
+    // Any required script can be added here
+  </script>
+  
+  <div class="container mx-auto p-4">
+    <h1 class="text-4xl font-bold text-center mb-6">System Design</h1>
+    
+    <div class="mb-8 p-4 bg-green-100 rounded-lg shadow-md">
+        <h2 class="text-3xl font-semibold mb-4">Smartphone Application</h2>
+        
+        <h3 class="text-2xl font-semibold">Security Handling</h3>
+        <p>The smartphone application will employ state-of-the-art encryption protocols for both data transmission and storage. To ensure the highest level of security, the app will use AES-256 encryption, which is the gold standard for data protection. Furthermore, all communication between the VR headset and the smartphone app will be secured via SSL/TLS, safeguarding against any data breaches or eavesdropping.</p>
+      
+        <h3 class="text-2xl font-semibold">Data Storing</h3>
+        <p>Data will be securely stored in the cloud, accessible only to authorized users. The app will employ OAuth 2.0 for user authentication, ensuring that only the legitimate user has access to their data. Furthermore, the application will support two-factor authentication for added security. All stored data will be anonymized and encrypted, providing both security and privacy to the user. The app will also feature customizable privacy settings, allowing users to control the sharing of their health data.</p>
+      
+        <h3 class="text-2xl font-semibold">User Interface & Experience</h3>
+        <p>The application will feature an intuitive and user-friendly interface, designed to provide a seamless experience from onboarding to daily usage. Real-time syncing with the VR headset ensures that users have immediate access to their health metrics. The app will also include visualizations of health trends, personalized insights, and recommendations based on the user's biometric data.</p>
+      </div>
+      
+  
+    <!-- VR Headset Section -->
+    <div class="mb-8 p-4 bg-orange-100 rounded-lg shadow-md">
+        <h2 class="text-3xl font-semibold mb-4">VR Headset</h2>
+        
+        <h3 class="text-2xl font-semibold">Security Handling</h3>
+        <p>The VR headset is designed with security as a priority. It features secure Bluetooth LE (Low Energy) connections to ensure efficient and secure communication with the smartphone application. Data channels between the headset and other devices are encrypted, preventing unauthorized access. The headset firmware will be regularly updated to address any potential security vulnerabilities and maintain compliance with the latest security standards.</p>
+      
+        <h3 class="text-2xl font-semibold">Design and Comfort</h3>
+        <p>The VR headset is ergonomically designed for comfort during prolonged use. It includes adjustable straps for a custom fit and is made with lightweight materials to reduce strain on the head and neck. The headset's interior is lined with a soft, breathable fabric for additional comfort. It is also designed to accommodate different face shapes and sizes, ensuring a snug and comfortable fit for a wide range of users.</p>
+      
+        <h3 class="text-2xl font-semibold">Advanced Features</h3>
+        <p>Along with standard VR capabilities, the headset incorporates advanced features such as adaptive focus for users with varying visual acuities and an ambient light sensor to adjust display brightness. It also includes built-in speakers and a microphone for a fully immersive experience. The headset is compatible with a range of VR content, making it suitable for both health monitoring and entertainment purposes.</p>
+      </div>
+      
+  
+    <!-- Bracelet Health Data Measurement Section -->
+    <div class="mb-8 p-4 bg-violet-100 rounded-lg shadow-md">
+      <h2 class="text-3xl font-semibold mb-4">Bracelet Health Data Measurement</h2>
+      <p>The device will be in the form of a bracelet that uses sensors to collect biometric data, then prepares and transmits it to the VR headset for display.</p>
+      <h3 class="text-2xl font-semibold">Bracelet Material</h3>
+      <p>Available in Nylon, Silicone, and vegan leather, each offering different price points and user experiences.</p>
+      <h3 class="text-2xl font-semibold">Heart-rate Sensor</h3>
+      <p>Utilizes a pulse photoplethysmography sensor integrated into the bracelet material to track heart rate accurately.</p>
+      <h3 class="text-2xl font-semibold">Breathing-rate Sensor</h3>
+      <p>An accelerometer woven into the bracelet material to monitor breathing rate effectively.</p>
+      <h3 class="text-2xl font-semibold">Transmitting Data</h3>
+      <p>Data will be transmitted using a Bluetooth module, ensuring real-time updates to the VR headset.</p>
+      <h3 class="text-2xl font-semibold">Battery</h3>
+      <p>A lithium-ion rechargeable battery will be used, chargeable via USB-C for user convenience.</p>
+    </div>
+  </div>
+  
+  <style>
+    /* Additional styling as needed */
+    .section {
+      background-color: #f9f9f9;
+      border-radius: 10px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+      padding: 20px;
+      margin-bottom: 20px;
+    }
+  
+    .section-title {
+      font-size: 1.5rem;
+      margin-bottom: 15px;
+    }
+  
+    .section-content {
+      font-size: 1rem;
+      line-height: 1.5;
+    }
+  
+    .section-content h3 {
+      font-size: 1.25rem;
+      margin-top: 10px;
+    }
+  </style>
+  
--- src/routes/Test_Plan/+page.svelte
+++ src/routes/Test_Plan/+page.svelte
@@ -1,59 +1,81 @@
 <script>
-    // Any required script can be added here
-  </script>
+  // Any required script can be added here
+</script>
+
+<div class="container mx-auto p-4">
+  <h1 class="text-4xl font-bold text-center mb-6">Test Plan</h1>
   
-  <div class="container mx-auto p-4">
-    <h1 class="text-4xl font-bold text-center mb-6">Test Plan</h1>
-    
-    <!-- Function Tests -->
-    <div class="mb-8">
-      <h2 class="text-3xl font-semibold mb-4">Function Tests</h2>
-      
-      <!-- Heart-rate Test -->
-      <div class="mb-4">
-        <h3 class="text-2xl font-semibold">Heart-rate Test</h3>
-        <p>Using a medical-grade oximeter for calibration, the heart-rate will be tested from 50 bpm to 165 bpm in both simulated and real-world conditions.</p>
-        <p><strong>Success Criteria:</strong> Device matches or is within ±5 bpm of the oximeter; alerts at 120+ or less than 50 bpm.</p>
-      </div>
+  <!-- Function Tests Section -->
+  <div class="mb-8">
+    <h2 class="text-3xl font-semibold text-center mb-4">Function Tests</h2>
+
+    <!-- Test Sections -->
+    <div class="test-section mb-4">
+      <h3 class="test-title">Heart-rate Test</h3>
+      <p class="test-description">Using a medical-grade oximeter for calibration, the heart-rate will be tested from 50 bpm to 165 bpm in both simulated and real-world conditions.</p>
+      <p class="test-criteria"><strong>Success Criteria:</strong> Device matches or is within ±5 bpm of the oximeter; alerts at 120+ or less than 50 bpm.</p>
+    </div>
   
       <!-- Breathing-rate Test -->
-      <div class="mb-4">
-        <h3 class="text-2xl font-semibold">Breathing-rate Test</h3>
-        <p>Conducted with a medical-grade capaciflector at rest, during activity, and intense activity.</p>
-        <p><strong>Success Criteria:</strong> Device matches or is within ±4 breaths per minute; safety alerts below 10 or above 25 breaths per minute.</p>
+      <div class="test-section mb-4">
+        <h3 class="test-title">Breathing-rate Test</h3>
+        <p class="test-description">Conducted with a medical-grade capaciflector at rest, during activity, and intense activity.</p>
+        <p class="test-criteria"><strong>Success Criteria:</strong> Device matches or is within ±4 breaths per minute; safety alerts below 10 or above 25 breaths per minute.</p>
       </div>
   
       <!-- Water Resistance Test -->
-      <div class="mb-4">
-        <h3 class="text-2xl font-semibold">Water Resistance Test</h3>
-        <p>25 mL of water splashed at the device from various angles to test operational stability.</p>
-        <p><strong>Success Criteria:</strong> Full operational capability during and after the test.</p>
+      <div class="test-section mb-4">
+        <h3 class="test-title">Water Resistance Test</h3>
+        <p class="test-description">25 mL of water splashed at the device from various angles to test operational stability.</p>
+        <p class="test-criteria"><strong>Success Criteria:</strong> Full operational capability during and after the test.</p>
       </div>
   
       <!-- Temperature Under Load Test -->
-      <div class="mb-4">
-        <h3 class="text-2xl font-semibold">Temperature Under Load Test</h3>
-        <p>Testing with simulated real-world activity at various intervals to monitor temperature.</p>
-        <p><strong>Success Criteria:</strong> Device temperature stays below 35 degrees C.</p>
+      <div class="test-section mb-4">
+        <h3 class="test-title">Temperature Under Load Test</h3>
+        <p class="test-description">Testing with simulated real-world activity at various intervals to monitor temperature.</p>
+        <p class="test-criteria"><strong>Success Criteria:</strong> Device temperature stays below 35 degrees C.</p>
       </div>
   
       <!-- Exercise Tests -->
-      <div class="mb-4">
-        <h3 class="text-2xl font-semibold">Exercise Tests</h3>
-        <p>Five different exercises conducted by individuals of varying abilities to assess comfort and fit.</p>
-        <p><strong>Success Criteria:</strong> At least 80% positive responses regarding comfort and fit.</p>
+      <div class="test-section mb-4">
+        <h3 class="test-title">Exercise Tests</h3>
+        <p class="test-description">Five different exercises conducted by individuals of varying abilities to assess comfort and fit.</p>
+        <p class="test-criteria"><strong>Success Criteria:</strong> At least 80% positive responses regarding comfort and fit.</p>
       </div>
   
       <!-- EMC Tests -->
-      <div class="mb-4">
-        <h3 class="text-2xl font-semibold">EMC Tests</h3>
-        <p>EMC testing and certification by UL Solutions.</p>
-        <p><strong>Success Criteria:</strong> Obtaining EMC certification.</p>
+      <div class="test-section mb-4">
+        <h3 class="test-title">EMC Tests</h3>
+        <p class="test-description">EMC testing and certification by UL Solutions.</p>
+        <p class="test-criteria"><strong>Success Criteria:</strong> Obtaining EMC certification.</p>
       </div>
     </div>
   </div>
   
   <style>
-    /* Additional styling as needed */
-  </style>
+    .test-section {
+      background-color: #f9f9f9; /* Light gray background */
+      border-radius: 12px; /* Rounded corners */
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Subtle shadow */
+      padding: 20px; /* Spacing inside each section */
+      margin-bottom: 20px; /* Spacing between sections */
+    }
   
+    .test-title {
+      font-size: 1.5rem; /* Larger font size for titles */
+      color: #333; /* Darker text for emphasis */
+      margin-bottom: 10px; /* Spacing after titles */
+    }
+  
+    .test-description {
+      font-size: 1.1rem; /* Slightly larger font size for readability */
+      margin-bottom: 15px; /* Spacing after description paragraph */
+    }
+  
+    .test-criteria {
+      font-size: 1.1rem; /* Slightly larger font size for criteria */
+      font-weight: 600; /* Semi-bold font weight for criteria */
+      color: #555; /* Dark gray for criteria text */
+    }
+  </style>
